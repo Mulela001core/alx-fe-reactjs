763a6ccbafb3663f45a71878553646f67e88b686
Mulela <emmanuelmulela@gmail.com>
2026-01-14 18:03:57 +0300

    feat: add Counter component and integrate it into App

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,3 +1,4 @@
+import Counter from './components/counter';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header'; 
 import MainContent from './components/MainContent';
@@ -38,6 +39,7 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+      <Counter />
 
 
       <Footer />
