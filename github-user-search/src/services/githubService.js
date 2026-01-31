364a983f6f7f510be112187dffb59c9407a77f62
Mulela <emmanuelmulela@gmail.com>
2026-01-31 20:24:02 +0300

    feat: enhance Search component with location and min repos filters, implement pagination, and update styling

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,29 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
+const BASE_URL = "https://api.github.com/search/users";
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`${BASE_URL}/users/${username}`, {
+export const searchUsers = async ({
+  query,
+  location,
+  minRepos,
+  page = 1,
+}) => {
+  let q = query || "";
+
+  if (location) {
+    q += ` location:${location}`;
+  }
+
+  if (minRepos) {
+    q += ` repos:>=${minRepos}`;
+  }
+
+  const response = await axios.get(BASE_URL, {
+    params: {
+      q,
+      page,
+      per_page: 10,
+    },
     headers: {
       Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
     },
