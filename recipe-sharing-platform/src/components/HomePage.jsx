bc51e7d121bce951562b137d884cbb55672c1935
Mulela <emmanuelmulela@gmail.com>
2026-02-12 17:22:34 +0300

    added "grid-cols-1"

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ export default function HomePage() {
         Recipe Collection 🍽️
       </h1>
 
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
