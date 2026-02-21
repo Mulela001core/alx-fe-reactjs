2c44d44af8b1ab3942ad5d4cdaf3bcf3ba325d02
Mulela <emmanuelmulela@gmail.com>
2026-02-21 13:26:49 +0300

    feat: add BlogPost and ProtectedRoute components with routing integration

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,19 +1,40 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import BlogPost from "./components/Blogpost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> |{" "}
-        <Link to="/profile">Profile</Link>
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/1">Blog Post 1</Link>
       </nav>
+
+      <button onClick={() => setIsAuthenticated(true)}>
+        Simulated Login
+      </button>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        { /* for nested routes */}
-        <Route path="/profile/*" element={<Profile />} />
+
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Dynamic route for blog posts */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
